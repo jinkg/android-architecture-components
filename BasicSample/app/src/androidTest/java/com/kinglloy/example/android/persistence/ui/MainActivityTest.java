2cb49e2386862f10d0b2b9bcca1272f58789f0c2
jinyalin <jinyalin@baidu.com>
2018-01-19 15:36:11 +0800

    add MainActivity espresso test

---
+++ BasicSample/app/src/androidTest/java/com/kinglloy/example/android/persistence/ui/MainActivityTest.java
@@ -0,0 +1,88 @@
+package com.kinglloy.example.android.persistence.ui;
+
+import android.arch.core.executor.testing.CountingTaskExecutorRule;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
+import android.support.annotation.Nullable;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+
+import com.kinglloy.example.android.persistence.AppExecutors;
+import com.kinglloy.example.android.persistence.EspressoTestUtil;
+import com.kinglloy.example.android.persistence.R;
+import com.kinglloy.example.android.persistence.db.AppDatabase;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+
+/**
+ * @author jinyalin
+ * @since 2018/1/19.
+ */
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Rule
+    public CountingTaskExecutorRule mCountingTaskExecutorRule = new CountingTaskExecutorRule();
+
+    public MainActivityTest() {
+        // delete the database
+        InstrumentationRegistry.getTargetContext().deleteDatabase(AppDatabase.DATABASE_NAME);
+    }
+
+    @Before
+    public void disableRecyclerViewAnimations() {
+        // Disable RecyclerView animations
+        EspressoTestUtil.disableAnimations(mActivityRule);
+    }
+
+    @Before
+    public void waitForDbCreation() throws Throwable {
+        final CountDownLatch latch = new CountDownLatch(1);
+        final LiveData<Boolean> databaseCreated = AppDatabase.getInstance(
+                InstrumentationRegistry.getTargetContext(), new AppExecutors())
+                .getDatabaseCreated();
+        mActivityRule.runOnUiThread(() -> databaseCreated.observeForever(new Observer<Boolean>() {
+            @Override
+            public void onChanged(@Nullable Boolean aBoolean) {
+                if (Boolean.TRUE.equals(aBoolean)) {
+                    databaseCreated.removeObserver(this);
+                    latch.countDown();
+                }
+            }
+        }));
+        MatcherAssert.assertThat("database should've initialized",
+                latch.await(1, TimeUnit.MINUTES), CoreMatchers.is(true));
+    }
+
+    @Test
+    public void clickOnFirstItem_opensComments() throws Throwable {
+        drain();
+        // When clicking on the first product
+        onView(withContentDescription(R.string.cd_products_list))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        drain();
+//        onView(withContentDescription(R.string.comm))
+    }
+
+    private void drain() throws TimeoutException, InterruptedException {
+        mCountingTaskExecutorRule.drainTasks(1, TimeUnit.MINUTES);
+    }
+}
